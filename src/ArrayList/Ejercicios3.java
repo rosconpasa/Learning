5086302dc442f8289eb5019d19fbdd7cf0d80ca6
rosconpasa <willdozawill@gmail.com>
2024-08-19 20:40:17 -0500

    Se adaptó a generics los métodos del paquete ArrayList

--- src/ArrayList/Ejercicios3.java
+++ src/ArrayList/Ejercicios3.java
@@ -9,41 +9,42 @@ public class Ejercicios3 {
     String cad;
     Scanner sc = new Scanner(System.in);
 
-
-    public void agregar(ArrayList<String> cadena) {
-        System.out.println("Ingresa un elemento");
-        cadena.add(sc.nextLine());
+    public <T> void agregar(ArrayList<T> elementos) {
+        System.out.println("Ingrese un elemento");
+        T elem = (T) sc.nextLine();
+        elementos.add(elem);
     }
 
-    public void imprimir(ArrayList<String> cadena) {
-        for (int i = 0; i < cadena.size(); i++) {
-            System.out.print(cadena.get(i) + ", ");
+    public <T> void imprimir(ArrayList<T> elementos) {
+        for (int i = 0; i < elementos.size(); i++) {
+            System.out.print(elementos.get(i) + ", ");
         }
         System.out.println("");
     }
 
-    public void eliminarPorPosición(ArrayList<String> cadena) {
+
+    public <T> void eliminarPorPosición(ArrayList<T> elementos) {
         System.out.println("Ingrese la posicion a eliminar");
         pos = sc.nextInt();
-        cadena.remove(pos);
-        System.out.println("Elemento " + cadena.get(pos) + " eliminado");
+        elementos.remove(pos);
+        System.out.println("Elemento " + elementos.get(pos) + " eliminado");
     }
 
-    public void buscarPalabra(ArrayList<String> cadena) {
-        System.out.println("Ingrese la palabra a buscar");
+    public <T> void buscarElemento(ArrayList<T> elementos) {
+        System.out.println("Ingrese el elemento a buscar");
         cad = sc.nextLine();
         boolean found = false;
         int i = 0;
         do {
-            if (cadena.get(i).equals(cad)) { // Si son iguales
+            if (elementos.get(i).equals(cad)) { // Si son iguales
                 found = true;
-                System.out.println("Palabra encontrada en la posición " + i + " del arrayList");
+                System.out.println("Elemento encontrada en la posición " + i + " del arrayList");
                 break;
             }
             i++;
-        } while (i<cadena.size()); //mientras que i sea menor que el tamaño del arrayList
-        if (!found){ // si es falso
-            System.out.println("Palabra no encontrada");
+        } while (i < elementos.size()); //mientras que i sea menor que el tamaño del arrayList
+        if (!found) { // si es falso
+            System.out.println("Elemento no encontrado");
         }
     }
 }
--- src/ArrayList/Menu_Tres.java
+++ src/ArrayList/Menu_Tres.java
@@ -1,7 +1,9 @@
 package ArrayList;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Scanner;
+import java.util.Collections;
 
 public class Menu_Tres {
     Scanner sc = new Scanner(System.in);
@@ -9,37 +11,90 @@ public class Menu_Tres {
     public void menuTres() {
         int op;
         int pos;
+        int tipo;
         Ejercicios3 ejercicios3 = new Ejercicios3();
         ArrayList<String> cadena = new ArrayList<>();
+        ArrayList<Integer> numeros = new ArrayList<>();
+
 
         do {
             System.out.println("---------MENÚ TRES---------\n" +
-                    "1. Agregar\n" +
+                    "1. Agregar elemento\n" +
                     "2. Imprimir\n" +
-                    "3. Eliminar por posición\n" +
+                    "3. Eliminar por posición \n" +
+                    "4. Buscar elemento \n" +
+                    "5. Ordenar elementos \n" +
                     "0. Volver al menú principal");
             op = sc.nextInt();
 
             switch (op) {
                 case 1:
-                    ejercicios3.agregar(cadena);
+                    System.out.println("Con que tipo de arrayList desea trabajar: 1. String, 2. Integer");
+                    tipo = sc.nextInt();
+
+                    if (tipo == 1) {
+                        ejercicios3.agregar(cadena);
+                    } else {
+                        ejercicios3.agregar(numeros);
+                    }
+
                     break;
 
                 case 2:
-                    ejercicios3.imprimir(cadena);
+
+                    System.out.println("Con que tipo de arrayList desea trabajar: 1. String, 2. Integer");
+                    tipo = sc.nextInt();
+
+                    if (tipo == 1) {
+                        ejercicios3.imprimir(cadena);
+                    } else {
+                        ejercicios3.imprimir(numeros);
+                    }
                     break;
 
                 case 3:
                     //Implementa un programa que permita al usuario agregar números a un ArrayList. Luego, pide al
                     // usuario eliminar un número ya sea por valor o por posición.
-                    ejercicios3.eliminarPorPosición(cadena);
+                    System.out.println("Con que tipo de arrayList desea trabajar: 1. String, 2. Integer");
+                    tipo = sc.nextInt();
+
+                    if (tipo == 1) {
+                        ejercicios3.eliminarPorPosición(cadena);
+                    } else {
+                        ejercicios3.eliminarPorPosición(numeros);
+                    }
 
                     break;
 
                 case 4:
                     //Crea un programa que permita agregar palabras a un ArrayList. Luego, pide al usuario ingresar una
                     // palabra y busca si existe en la lista, mostrando su posición.
-                    ejercicios3.buscarPalabra(cadena);
+
+                    System.out.println("Con que tipo de arrayList desea trabajar: 1. String, 2. Integer");
+                    tipo = sc.nextInt();
+
+                    if (tipo == 1) {
+                        ejercicios3.buscarElemento(cadena);
+                    } else {
+                        ejercicios3.buscarElemento(numeros);
+                    }
+                    break;
+
+                case 5:
+                    //Escribe un programa que permita al usuario agregar números a un ArrayList, luego ordena los
+                    // números en orden ascendente y muestra la lista ordenada.
+
+                    System.out.println("Con que tipo de arrayList desea trabajar: 1. String, 2. Integer");
+                    tipo = sc.nextInt();
+
+                    if (tipo == 1) {
+                        Collections.sort(cadena);
+                        ejercicios3.imprimir(cadena);
+                    } else {
+                        Collections.sort(numeros);
+                        ejercicios3.imprimir(numeros);
+                    }
+
                     break;
 
                 case 0:
